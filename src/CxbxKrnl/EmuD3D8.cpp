5b379699dceb43dc44c9b051cb268e863c354782
PatrickvL <pvanlogchem@gmail.com>
2017-07-27 17:13:25 +0200

    Moved note on __thiscall patching towards Miniport (where it's actually applied)

--- src/CxbxKrnl/EmuD3D8.cpp
+++ src/CxbxKrnl/EmuD3D8.cpp
@@ -2584,19 +2584,6 @@ void CxbxUpdateActiveIndexBuffer
 	#undef PATCH_PUSHBUFFER
 #endif
 
-// Note on __thiscall vs __fastcall :
-//
-// Class::functions are compiled using the __thiscall calling convention.
-//
-// Our patches are not class functions, so we need to cater for two things :
-// 1) An extra initial This pointer argument
-// 2) Usage of __thiscall isn't allowed on not-class-functions.
-// Ad 1 can be easily fixed by inserting an initial This argument to each argument list
-// Ad 2 can be fixed using the __fastcall calling convention instead,
-// *but* insert an extra argument to make up for the unused EDX register.
-// Source of this solution :
-// https ://www.unknowncheats.me/forum/617803-post5.html?s=e4e45c25853735b088016787354a8ad5
-
 HRESULT WINAPI XTL::EMUPATCH(Direct3D_CreateDevice)
 (
     UINT                        Adapter,
