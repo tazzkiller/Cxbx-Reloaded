e25b7f4d0b703dd28a76e2ff2f0206ac3c04ef2f
PatrickvL <pvanlogchem@gmail.com>
2017-11-09 17:42:48 +0100

    Reserved lowest 128 Mb using a placeholder again (but without taking executable space). No need to remember the reservation results. Verified and documented all address ranges that may fail. Additional comments.

--- src/Cxbx-Loader/loader.cpp
+++ src/Cxbx-Loader/loader.cpp
@@ -1,8 +1,57 @@
-// Based on https://stackoverflow.com/questions/39217241/visual-studio-2015-compile-c-c-without-a-runtime-library/39220245#39220245
-
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+// ******************************************************************
+// *
+// *    .,-:::::    .,::      .::::::::.    .,::      .:
+// *  ,;;;'````'    `;;;,  .,;;  ;;;'';;'   `;;;,  .,;;
+// *  [[[             '[[,,[['   [[[__[[\.    '[[,,[['
+// *  $$$              Y$$$P     $$""""Y$$     Y$$$P
+// *  `88bo,__,o,    oP"``"Yo,  _88o,,od8P   oP"``"Yo,
+// *    "YUMMMMMP",m"       "Mm,""YUMMMP" ,m"       "Mm,
+// *
+// *   Common->ReservedMemory.h
+// *
+// *  This file is part of the Cxbx project.
+// *
+// *  Cxbx is free software; you can redistribute it
+// *  and/or modify it under the terms of the GNU General Public
+// *  License as published by the Free Software Foundation; either
+// *  version 2 of the license, or (at your option) any later version.
+// *
+// *  This program is distributed in the hope that it will be useful,
+// *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+// *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// *  GNU General Public License for more details.
+// *
+// *  You should have recieved a copy of the GNU General Public License
+// *  along with this program; see the file COPYING.
+// *  If not, write to the Free Software Foundation, Inc.,
+// *  59 Temple Place - Suite 330, Bostom, MA 02111-1307, USA.
+// *
+// *  (c) 2017 Patrick van Logchem <pvanlogchem@gmail.com>
+// *
+// *  All rights reserved
+// *
+// ******************************************************************
 #include <Windows.h> // For DWORD, CALLBACK, VirtualAlloc, LPVOID, SIZE_T, HMODULE 
 
-#include "..\Common\XboxAddressRanges.h"
+#include "..\Common\XboxAddressRanges.h" // For XboxAddressRangeType, XboxAddressRanges
+
+// Reserve the first 128 MB MemLowVirtual address range without inflating the EXE size,
+// by simply declaring an array, first thing, in global scope. It gets placed in the BSS segment,
+// so it still uses space in RAM, but not the actual executable.
+// This variable *MUST* be this large, for it to take up address space
+// so that all other code and data in this module are placed outside of the
+// maximum virtual memory range.
+#define VM_PLACEHOLDER_SIZE 128 * 1024 * 1024
+
+// Note : In the old setup, we used #pragma section(".text"); __declspec(allocate(".text"))
+// to put this variable at the exact image base address 0x00010000, but that resulted in
+// an increase of the executable by 128 Mb. Since we won't return to the tiny loader code,
+// and the executable doesn't contain any data that we refer too once we entered the emulation DLL,
+// this will be all right. The only bit of data I transfer over, is on the stack, but the stack
+// (of the initial thread) resides far outside of the reserved range, so that's all right too.
+unsigned char virtual_memory_placeholder[VM_PLACEHOLDER_SIZE] = { 0 }; // = { OPCODE_NOP_90 };
 
 // Maps an XboxAddressRangeType to page protection flags, for use by VirtualAlloc
 DWORD XboxAddressRangeTypeToVirtualAllocPageProtectionFlags(XboxAddressRangeType type)
@@ -35,47 +84,43 @@ DWORD XboxAddressRangeTypeToVirtualAllocPageProtectionFlags(XboxAddressRangeType
   ((sizeof(a) / sizeof(*(a))) /                     \
   static_cast<size_t>(!(sizeof(a) % sizeof(*(a)))))
 
+// Note : This executable is meant to be as tiny as humanly possible.
+// The C++ runtime library is removed using https://stackoverflow.com/a/39220245/12170
+
 DWORD CALLBACK rawMain()
 {
-	// Reserve space for the result of reserving all address ranges
-	LPVOID VirtualAllocResults[ARRAY_SIZE(XboxAddressRanges)];
-
-//Sleep(1000 * 60); // DEBUG : One minute to take an initial snapshot using SysInternals VMMap
-
 	// Loop over all Xbox address ranges
 	for (int i = 0; i < ARRAY_SIZE(XboxAddressRanges); i++) {
-		// Low memory needs special treatment
-		xbaddr Start = XboxAddressRanges[i].Start;
-		if (XboxAddressRanges[i].Type == MemLowVirtual)
-			Start = 0x00040000; // TODO : Read this from image header
-
-		// Reserve this address range, and store the result
-		VirtualAllocResults[i] = VirtualAlloc(
-			(LPVOID)Start,
+		// Try to reserve each address range
+		if (nullptr == VirtualAlloc(
+			(LPVOID)XboxAddressRanges[i].Start,
 			(SIZE_T)XboxAddressRanges[i].Size,
 			MEM_RESERVE,
 			XboxAddressRangeTypeToVirtualAllocPageProtectionFlags(XboxAddressRanges[i].Type)
-		);
+		)) {
+			// Ranges that fail under Windows 7 Wow64 :
+			//
+			// { MemLowVirtual, 0x00000000, MB(128) }, // .. 0x08000000 (Retail Xbox uses 64 MB)
+			// { DeviceUSB1,    0xFED08000, KB(  4) }, // .. 0xFED09000
+			// { DeviceFlash,   0xFFC00000, MB(  4) }, // .. 0xFFFFFFFF (Flash mirror 4) - Will probably fail reservation
+			// { DeviceMCPX,    0xFFFFFE00,    512  }, // .. 0xFFFFFFFF (not Chihiro, Xbox - if enabled)
+			//
+			// .. none of which are an issue for now.
+			switch (XboxAddressRanges[i].Type) {
+			case MemLowVirtual: // Already reserved via virtual_memory_placeholder
+			case DeviceUSB1: // Won't be emulated for a long while
+			case DeviceMCPX: // Can safely be ignored
+				continue;
+			case DeviceFlash: // Losing mirror 4 is acceptable - the 3 others work just fine
+				if (XboxAddressRanges[i].Start == 0xFFC00000)
+					continue;
+			}
+
+			// If we get here, emulation lacks important address ranges; Don't launch
+			OutputDebugString(L"Required address range couldn't be reserved!");
+			return ERROR_NOT_ENOUGH_MEMORY;
+		}
 	}
-
-	// Note : Ranges that failed under Windows 7 Wow64 :
-	//
-	// { DeviceUSB1, 0xFED08000, KB(4) }, // .. 0xFED09000
-	// { DeviceFlash,   0xFFC00000, MB(4) }, // .. 0xFFFFFFFF (Flash mirror 4) - Will probably fail reservation
-	// { DeviceMCPX,    0xFFFFFE00,    512 }, // .. 0xFFFFFFFF (not Chihiro, Xbox - if enabled)
-	//
-	// .. none of which are an issue for now.
-	// 
-	// More problematic however, is this failure :
-	//
-	// { MemLowVirtual, 0x00000000, MB(128) }, // .. 0x08000000 (Retail Xbox uses 64 MB)
-	//
-	// SysInternals VMMap shows there are a few 'Unusable' pages in there, a DLL,
-	// thread stack, private and shared data, all in the lower 128 MB....
-	//
-	// TODO : We *really* need to fix that, otherwise the loaded-approach has little added value.
-
-Sleep(1000 * 60 * 5); // DEBUG : Five minutes to look at the result in SysInternals VMMap
 
 	// Only after the required memory ranges are reserved, load our emulation DLL
 	HMODULE hEmulationDLL = LoadLibrary(L"Cxbx-Reloaded.DLL");
@@ -83,11 +128,12 @@ Sleep(1000 * 60 * 5); // DEBUG : Five minutes to look at the result in SysIntern
 		return ERROR_RESOURCE_NOT_FOUND;
 
 	// Find the main emulation function in our DLL
-	typedef DWORD (WINAPI * Emulate_t)(LPVOID *);
+	typedef DWORD (WINAPI * Emulate_t)();
 	Emulate_t pfnEmulate = (Emulate_t)GetProcAddress(hEmulationDLL, "Emulate");
 	if(!pfnEmulate)
 		return ERROR_RESOURCE_NOT_FOUND;
 
-	// Call the main emulation function in our DLL
-	return pfnEmulate(VirtualAllocResults);
+	// Call the main emulation function in our DLL, passing in the results
+	// of the address range reservations
+	return pfnEmulate(); // TODO : Pass along the command line
 }
