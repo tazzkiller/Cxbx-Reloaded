b9136ea980ae5b47bdd432d8e9121df8dd5e8bfc
PatrickvL <pvanlogchem@gmail.com>
2017-11-16 14:07:38 +0100

    Renamed szFilePath_CxbxReloaded_Exe into szFilePath_CxbxLoader_Exe

--- src/CxbxKrnl/CxbxKrnl.cpp
+++ src/CxbxKrnl/CxbxKrnl.cpp
@@ -89,7 +89,7 @@ Xbe::Certificate *g_pCertificate = NULL;
 /*! thread handles */
 static HANDLE g_hThreads[MAXIMUM_XBOX_THREADS] = { 0 };
 
-char szFilePath_CxbxReloaded_Exe[MAX_PATH] = { 0 };
+char szFilePath_CxbxLoader_Exe[MAX_PATH] = { 0 };
 char szFolder_CxbxReloadedData[MAX_PATH] = { 0 };
 char szFilePath_LaunchDataPage_bin[MAX_PATH] = { 0 };
 char szFilePath_EEPROM_bin[MAX_PATH] = { 0 };
@@ -1059,7 +1059,7 @@ void CxbxInitFilePaths()
 	snprintf(szFilePath_EEPROM_bin, MAX_PATH, "%s\\EEPROM.bin", szFolder_CxbxReloadedData);
 	snprintf(szFilePath_memory_bin, MAX_PATH, "%s\\memory.bin", szFolder_CxbxReloadedData);
 
-	GetModuleFileName(GetModuleHandle(NULL), szFilePath_CxbxReloaded_Exe, MAX_PATH);
+	GetModuleFileName(GetModuleHandle(NULL), szFilePath_CxbxLoader_Exe, MAX_PATH);
 }
 
 // TODO : Is DefaultLaunchDataPage really necessary? (No-one assigns it yet to LaunchDataPage)
